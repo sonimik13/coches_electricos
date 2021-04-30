bdacbf314b54788f20536bf03c3209e99e29af48
ConradMeyer <meyeralten@gmail.com>
2021-04-30 19:47:38 +0200

    starting with front

--- fsFront/src/App.js
+++ fsFront/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Signin from './components/Signin/Signin'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <Menu />
+          <Switch>
+            <Route path="/login" component={Signin} />
+            <Route exact path="/">
+              <Main />
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ fsFront/src/components/Signin/Signin.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+
+function Signin (props) {
+    const [state, setState] = useState("")
+
+    return (
+        <div className="login">
+            <img src="" alt="logo"/>
+            <input type="text"/>
+            <input type="password"/>
+            <button></button>
+            <button></button>
+        </div>
+    )
+}
