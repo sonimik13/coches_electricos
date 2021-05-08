1c1bd7bb79277be0b7c2acdb59d8560eb4c18b0e
JoseMarin <josem_marin@hotmail.com>
2021-05-08 13:26:14 +0200

    userinfo6.7

--- fsFront/src/App.js
+++ fsFront/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import Home from './components/Home/Home'
+import UserInfo from './components/CargasRapidas/UserInfo'
+import Carga1 from "./components/CargasRapidas/Carga1/Carga1"
+import Carga2 from "./components/CargasRapidas/Carga2/Carga2"
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <UserInfo />
+      {/* <Carga1 /> */}
+      {/* <Carga2 />  */}
+        
     </div>
   );
 }
