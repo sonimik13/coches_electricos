a9a120c49fc324d0973076416249dca5f668a9dc
Gonzalo Alarcon <gonzandres_39@hotmail.com>
2021-05-11 11:14:43 +0200

    Facebook

--- fsFront/src/Components/FacebookLogin/facebook.js
+++ fsFront/src/Components/FacebookLogin/facebook.js
@@ -12,6 +12,7 @@ class facebook extends React.Component {
         return (
             <FacebookLogin
                 appId="180266487194624"
+                appSecret="68d41c54faa5a8aa42c995a59da44ccf"
                 autoLoad={true}
                 fields="name,email,picture"
                 callback={this.responseFacebook}
