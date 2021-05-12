4766015969fc80d7726b9380c608526a7f77ed30
Gonzalo Alarcon <gonzandres_39@hotmail.com>
2021-05-12 09:31:04 +0200

    Faceboob

--- fsFront/src/Components/FacebookLogin/facebook.js
+++ fsFront/src/Components/FacebookLogin/facebook.js
@@ -25,7 +25,7 @@ const Facebook = () => {
             <br />
 
             <script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v10.0&appId=180266487194624&autoLogAppEvents=1" nonce="LMlaoHI4"></script>
-            <div class="fb-login-button" data-width="" data-size="large" data-button-type="login_with" data-layout="rounded" data-auto-logout-link="false" data-use-continue-as="true"></div> </div>
+            <div className="fb-login-button" data-width="" data-size="large" data-button-type="login_with" data-layout="rounded" data-auto-logout-link="false" data-use-continue-as="true"></div> </div>
     )
 }
 
